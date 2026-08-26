c145de30465450ea60080ebb7cb11147698018bc
Cherry <949886@qq.com>
2026-08-27 03:10:30 +0800

    Resolve standalone dependencies from Endfield VFS

--- Source/AssetRipper.Import/Structure/GameInitializer.StructureDependencyProvider.cs
+++ Source/AssetRipper.Import/Structure/GameInitializer.StructureDependencyProvider.cs
@@ -2,7 +2,9 @@
 using AssetRipper.Import.Logging;
 using AssetRipper.Import.Structure.Platforms;
 using AssetRipper.IO.Files;
+using AssetRipper.IO.Files.Endfield;
 using AssetRipper.IO.Files.SerializedFiles.Parser;
+using AssetRipper.IO.Files.Streams.Smart;
 
 namespace AssetRipper.Import.Structure;
 
@@ -17,7 +19,45 @@ internal sealed partial record class GameInitializer
 		public FileBase? FindDependency(FileIdentifier identifier)
 		{
 			string? systemFilePath = RequestDependency(identifier.PathName);
-			return systemFilePath is null ? null : SchemeReader.LoadFile(systemFilePath, FileSystem);
+			if (systemFilePath is not null)
+			{
+				return SchemeReader.LoadFile(systemFilePath, FileSystem);
+			}
+
+			if (TryOpenEndfieldDependency(PlatformStructure, identifier.PathName, out FileBase? dependency)
+				|| TryOpenEndfieldDependency(MixedStructure, identifier.PathName, out dependency))
+			{
+				Logger.Info(LogCategory.Import, $"Dependency '{identifier.PathName}' has been loaded from Endfield VFS");
+				return dependency;
+			}
+
+			return null;
+		}
+
+		private static bool TryOpenEndfieldDependency(
+			PlatformGameStructure? structure,
+			string dependencyName,
+			[NotNullWhen(true)] out FileBase? dependency)
+		{
+			if (structure?.GameDataPath is not string gameDataPath
+				|| !EndfieldVfsCatalog.TryGet(gameDataPath, out EndfieldVfsCatalog? catalog)
+				|| !catalog.TryOpenFile(dependencyName, out Stream? stream, out string? virtualPath))
+			{
+				dependency = null;
+				return false;
+			}
+
+			SmartStream smartStream = SmartStream.Create(stream);
+			string fileName = GetVfsFileName(dependencyName);
+			dependency = SchemeReader.ReadFile(smartStream, virtualPath, fileName);
+			return true;
+		}
+
+		private static string GetVfsFileName(string value)
+		{
+			string normalized = value.Replace('\\', '/');
+			int slashIndex = normalized.LastIndexOf('/');
+			return slashIndex >= 0 ? normalized[(slashIndex + 1)..] : normalized;
 		}
 
 		/// <summary>
