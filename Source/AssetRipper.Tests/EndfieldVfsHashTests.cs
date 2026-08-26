2898e50c03617d98af5b88910d9243a3326b0fd8
Cherry <949886@qq.com>
2026-08-27 03:14:59 +0800

    Cover longer Endfield block hash names

--- Source/AssetRipper.Tests/EndfieldVfsHashTests.cs
+++ Source/AssetRipper.Tests/EndfieldVfsHashTests.cs
@@ -7,6 +7,8 @@ internal sealed class EndfieldVfsHashTests
 {
 	[TestCase(EndfieldVfsBlockType.Bundle, "7064D8E2")]
 	[TestCase(EndfieldVfsBlockType.InitialBundle, "0CE8FA57")]
+	[TestCase(EndfieldVfsBlockType.DynamicStreaming, "23D53F5D")]
+	[TestCase(EndfieldVfsBlockType.AuditDynamicStreaming, "B9358E30")]
 	public void KnownBlockDirectoryNamesMatchReference(EndfieldVfsBlockType blockType, string expected)
 	{
 		Assert.That(EndfieldVfsHash.GetBlockDirectoryName(blockType), Is.EqualTo(expected));
